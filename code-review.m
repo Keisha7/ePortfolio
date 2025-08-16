cd8093447e93a3bc1566a112cbd4fbf5eeae3f25
Keisha7 <141890217+Keisha7@users.noreply.github.com>
2025-08-16 11:37:12 -0400

    Create code-review.m

---
+++ code-review.m
@@ -0,0 +1,14 @@
+---
+layout: default
+title: "Code Review"
+---
+
+# Code Review Video
+
+**Video Link:** https://www.youtube.com/watch?v=TyiaB7RbCmk&ab_channel=KeishaYearwood%28Keisha%29
+
+In this code review, I walk through the original artifact, analyze its functionality, identify areas for improvement, and outline my planned enhancements across:
+
+- Software Design & Engineering  
+- Algorithms & Data Structures  
+- Databases  
